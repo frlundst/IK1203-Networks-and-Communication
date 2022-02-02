3e01ef2fcc9ef1bf3a57020e2a32b3fca82c30e4
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-02-02 14:15:06 +0100

    template

---
+++ task1/tcpclient/TCPClient.java
@@ -0,0 +1,13 @@
+package tcpclient;
+//import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+    
+    public TCPClient() {
+    }
+
+    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
+        return new byte[0];
+    }
+}
