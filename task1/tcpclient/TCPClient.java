197b613950801fac34b261f81378347b174fe77a
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-02-09 17:47:55 +0100

    updated

--- task1/tcpclient/TCPClient.java
+++ task1/tcpclient/TCPClient.java
@@ -7,12 +7,28 @@ public class TCPClient {
     public TCPClient() {
     }
 
+    /**
+     * This method receives an hostname and port and the client establishes a connection to that host.
+     * User input gets sent (a string that was encoded into byte form) through an outputstream.
+     * @param hostname
+     * @param port
+     * @param toServerBytes a byte array of the user input that is sent to a server.
+     * @return a byte array from a ByteArrayOutputStream that got the sockets inputsstream (in byte array form) written to it.
+     * @throws IOException
+     */
     public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
         try (Socket clientSocket = new Socket(hostname, port)) {
+            byte buffer[] = new byte[1024];
             OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write(toServerBytes);
             InputStream inputStream = clientSocket.getInputStream();
-            return inputStream.readAllBytes();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+            outputStream.write(toServerBytes);
+        
+            byteArrayOutputStream.write(buffer, 0, inputStream.read(buffer));
+            
+            clientSocket.close();
+            return byteArrayOutputStream.toByteArray();
         }
     }
 }
