c60f4931819207dd60b23569a876754086875db9
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-02-16 18:42:58 +0100

    task2

---
+++ task2/tcpclient/TCPClient.java
@@ -0,0 +1,50 @@
+package tcpclient;
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+    private boolean shutdown = false;
+    private Integer timeout = null;
+    private Integer limit = null;
+    
+    /**
+     * Constructor
+     * @param shutdown
+     * @param timeout
+     * @param limit
+     */
+    public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
+        this.shutdown = shutdown;
+        this.timeout = timeout;
+        this.limit = limit;
+    }
+
+    /**
+     * This method receives an hostname and port and the client establishes a connection to that host.
+     * User input gets sent (a string that was encoded into byte form) through an outputstream.
+     * @param hostname
+     * @param port
+     * @param toServerBytes a byte array of the user input that is sent to a server.
+     * @return a byte array from a ByteArrayOutputStream that got the sockets inputsstream (in byte array form) written to it.
+     * @throws IOException
+     */
+    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
+        Socket clientSocket = new Socket(hostname, port);
+
+        if(timeout != null) clientSocket.setSoTimeout(timeout);
+
+        byte buffer[] = new byte[1024];
+        OutputStream outputStream = clientSocket.getOutputStream();
+        InputStream inputStream = clientSocket.getInputStream();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        outputStream.write(toServerBytes);
+
+        for(int i = inputStream.read(buffer); i != -1; i = inputStream.read(buffer)){
+            byteArrayOutputStream.write(buffer, 0, i);
+        }
+
+        clientSocket.close();
+        return byteArrayOutputStream.toByteArray();
+    }
+}
